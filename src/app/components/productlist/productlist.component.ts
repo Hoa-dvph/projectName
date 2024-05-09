9514ccbe120c01f71f019bcaf3bbfc61395cb503
Hoa-dvph <150907011+Hoa-dvph@users.noreply.github.com>
2024-05-09 22:43:59 +0700

    lab-2

--- src/app/components/productlist/productlist.component.ts
+++ src/app/components/productlist/productlist.component.ts
@@ -1,12 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { IProduct } from '../../entities/products';
 
 @Component({
   selector: 'app-productlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './productlist.component.html',
   styleUrl: './productlist.component.css'
 })
-export class ProductlistComponent {
+export class ProductlistComponent implements OnInit {
+  products: IProduct[] = [
+    {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    },
+    {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    },
+    {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2015",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    }
+  ];
+  listProduct: IProduct[] = [];
+  filterValue: string = ''; // Từ khóa tìm kiếm
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Sao chép danh sách sản phẩm vào listProduct để giữ nguyên danh sách gốc
+    this.listProduct = [...this.products];
+  }
+
+  filter() {
+    // Lọc danh sách sản phẩm dựa trên từ khóa tìm kiếm và lưu kết quả vào một mảng mới
+    this.products = this.listProduct.filter(p =>
+      p.productName.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
+  }
 }
