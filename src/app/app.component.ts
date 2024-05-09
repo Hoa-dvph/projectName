838cb70bbc2ab29cb19c9e47a891f49876acef72
Hoa-dvph <150907011+Hoa-dvph@users.noreply.github.com>
2024-05-09 21:40:18 +0700

    lab1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,87 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Inventor } from './entities/inventor';
+import { IProduct } from './entities/products';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductlistComponent } from './components/productlist/productlist.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgFor, HeaderComponent, ProductlistComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'projectName';
+  title = 'FPT Polytechnic';
+  student = {
+    hoten: "Dương Văn Hòa",
+    gioitinh: "Nam",
+    ngaysinh: "06/08/2004",
+    anh: "avt.jpg",
+    diachi: "Vĩnh Phúc",
+    diemTB: 9.9
+  }
+  inventors: Inventor[] = [
+    { id: 1, first: "Duong", last: "D", year: 1998, passed: 1 },
+    { id: 2, first: "Tran", last: "A", year: 1942, passed: 0 },
+    { id: 3, first: "Nguyen", last: "B", year: 1934, passed: 1 },
+    { id: 4, first: "Phan", last: "C", year: 1945, passed: 1 },
+    { id: 5, first: "Dang", last: "E", year: 1995, passed: 1 }
+  ];
+  products: IProduct[] = [
+    {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    },
+    {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    },
+    {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2015",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    }
+  ];
 }
+
