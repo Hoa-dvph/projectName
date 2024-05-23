357518f5766085d2c758a2eb89a26d2156453a31
Hoa-dvph <150907011+Hoa-dvph@users.noreply.github.com>
2024-05-23 15:04:27 +0700

    demo lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { ProductlistComponent } from './components/productlist/productlist.component';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgFor, HeaderComponent, ProductlistComponent, FormsModule],
+  imports: [CommonModule, RouterOutlet, NgFor, HeaderComponent, ProductlistComponent, FormsModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
