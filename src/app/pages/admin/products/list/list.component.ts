afc7352cb33c94e065a5a2fc95ded6720de263f6
Hoa-dvph <150907011+Hoa-dvph@users.noreply.github.com>
2024-05-23 15:34:14 +0700

    khe

--- src/app/pages/admin/products/list/list.component.ts
+++ src/app/pages/admin/products/list/list.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ProductService } from '../../../../service/product.service';
 import { IProduct } from '../../../../entities/products';
 import { NgFor } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, RouterLink, FormsModule, NgxPaginationModule],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   products: IProduct[] = [];
+  listProduct: IProduct[] = [];
+  filterValue: string = '';
+  page: number = 1;
 
   productService = inject(ProductService);
 
   ngOnInit() {
     this.productService.getAllProducts().subscribe(
-      (products) => (this.products = products),
+      (products) => {
+        this.products = products;
+        this.listProduct = products;
+      },
       (error) => {
         console.error('Error fetching products:', error);
         alert('An error occurred while fetching the product list.');
@@ -32,13 +39,23 @@ export class ProductListComponent implements OnInit {
       this.productService.deleteProduct(product.id).subscribe(
         () => {
           this.products = this.products.filter(p => p !== product);
-          alert('Product deleted successfully!');
+          this.listProduct = this.listProduct.filter(p => p !== product);
         },
         (error) => {
           console.error('Error deleting product:', error);
           alert('An error occurred while deleting the product.');
         }
       );
+    }
+  }
+
+  filter() {
+    if (this.filterValue) {
+      this.products = this.listProduct.filter(p =>
+        p.productName.toLowerCase().includes(this.filterValue.toLowerCase())
+      );
+    } else {
+      this.products = [...this.listProduct];
     }
   }
 }
